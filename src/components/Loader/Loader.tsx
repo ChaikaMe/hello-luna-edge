878ce7b38b1560c6c447213e18775603c39e3dba
ClownTripleSrank <145134985+ChaikaMe@users.noreply.github.com>
2024-11-02 01:35:21 +0200

    Returning to old settings

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -10,7 +10,6 @@ export default function Loader() {
       ariaLabel="grid-loading"
       radius="12.5"
       wrapperStyle={{
-        padding: "40px",
         width: "100%",
         height: "100%",
         minHeight: "100%",
